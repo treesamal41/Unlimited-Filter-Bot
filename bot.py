7682c0030c57b6a1958fedb3aaea57a6698784f3
treesamal41 <treesamal41@gmail.com>
2026-09-22 07:02:04 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,18 +7,14 @@ else:
     from config import Config
 
 
-
-if __name__ == "__main__" :
-    plugins = dict(
-        root="plugins"
-    )
+if __name__ == "__main__":
+    plugins = dict(root="plugins")
     app = pyrogram.Client(
-        "filter bot",
+        "filter_bot",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins,
-        workers=300
+        workers=100,
     )
-    Config.AUTH_USERS.add(str(680815375))
     app.run()
